6a9289ac6c7e8b9beb3dd303f85b4de1cbb0956f
Jeremyhaglerr <jeremyhaglerr@gmail.com>
2022-03-28 17:54:05 -0500

    Fix error in classNames

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -5,30 +5,30 @@ const NavBar = ({ user, handleLogout }) => {
   return (
     <>
       {user ?
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <Link class="navbar-brand" to="/">GoLocal</Link>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <Link className="navbar-brand" to="/">GoLocal</Link>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNavDropdown">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <Link class="nav-link" to="/">Home<span class="sr-only">(current)</span></Link>
+  <div className="collapse navbar-collapse" id="navbarNavDropdown">
+    <ul className="navbar-nav">
+      <li className="nav-item active">
+        <Link className="nav-link" to="/">Home<span className="sr-only">(current)</span></Link>
       </li>
-      <li class="nav-item">
-        <Link class="nav-link" to="/create">Add Business</Link>
+      <li className="nav-item">
+        <Link className="nav-link" to="/create">Add Business</Link>
       </li>
-      <li class="nav-item">
-        <Link class="nav-link" to="/profile">Profile</Link>
+      <li className="nav-item">
+        <Link className="nav-link" to="/profile">Profile</Link>
       </li>
-      <li class="nav-item dropdown">
-        <Link class="nav-link dropdown-toggle" to="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+      <li className="nav-item dropdown">
+        <Link className="nav-link dropdown-toggle" to="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
           🛠
         </Link>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-          <Link class="dropdown-item" to="" onClick={handleLogout} >Sign Out</Link>
-          <Link class="dropdown-item" to="/changePassword">Change Password</Link>
-          <Link class="dropdown-item" to="#"></Link>
+        <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <Link className="dropdown-item" to="" onClick={handleLogout} >Sign Out</Link>
+          <Link className="dropdown-item" to="/changePassword">Change Password</Link>
+          <Link className="dropdown-item" to="#"></Link>
         </div>
       </li>
     </ul>
