412efe212668be8a0869022f06c17e3009248c7b
Jeremyhaglerr <jeremyhaglerr@gmail.com>
2022-03-31 18:19:24 -0500

    clean code and indent

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-
 import { useState, useEffect } from 'react'
 import { Routes, Route, useNavigate, Navigate } from 'react-router-dom'
 import NavBar from './components/NavBar/NavBar'
@@ -19,14 +18,14 @@ import ListDetails from './pages/ListDetails/ListDetails'
 
 const App = () => {
   const [user, setUser] = useState(authService.getUser())
-  const [businesses, setBusinesses]= useState([])
+  const [businesses, setBusinesses] = useState([])
   const [profile, setProfile] = useState([])
   const navigate = useNavigate()
 
-  useEffect(()=> {
-    if(user) {
+  useEffect(() => {
+    if (user) {
       businessService.getAll()
-      .then(allBusinesses => setBusinesses(allBusinesses))
+        .then(allBusinesses => setBusinesses(allBusinesses))
     }
   }, [user])
 
@@ -40,16 +39,16 @@ const App = () => {
     setUser(authService.getUser())
   }
 
-  useEffect(()=> {
-    if(user) {
+  useEffect(() => {
+    if (user) {
       businessService.getAll()
-      .then(allBusinesses => setBusinesses(allBusinesses))
+        .then(allBusinesses => setBusinesses(allBusinesses))
     }
   }, [user])
 
-  useEffect(()=> {
+  useEffect(() => {
     profileService.getProfile(user?.profile)
-    .then(profile => setProfile(profile))
+      .then(profile => setProfile(profile))
   }, [])
 
   const handleAddBusiness = newBusinessData => {
@@ -61,66 +60,65 @@ const App = () => {
   const handleAddReview = (newReviewData, business) => {
     businessService.createReview(newReviewData, business)
     businessService.getAll()
-    .then(allBusinesses => setBusinesses(allBusinesses))
+      .then(allBusinesses => setBusinesses(allBusinesses))
     window.location.reload(false)
   }
 
   const handleDeleteReview = (reviewId, businessId) => {
     businessService.deleteReview(reviewId, businessId)
     businessService.getAll()
-    .then(allBusinesses => setBusinesses(allBusinesses))
+      .then(allBusinesses => setBusinesses(allBusinesses))
     window.location.reload(false)
   }
 
   const handleEditBusiness = updatedBusinessData => {
     businessService.update(updatedBusinessData)
-    .then(updatedBusiness => {
-      const newBusinessArray = businesses.map(business => business._id === updatedBusiness._id ? updatedBusiness : business)
-      setBusinesses(newBusinessArray)
-      navigate('/')
-    })
-
+      .then(updatedBusiness => {
+        const newBusinessArray = businesses.map(business => business._id === updatedBusiness._id ? updatedBusiness : business)
+        setBusinesses(newBusinessArray)
+        navigate('/')
+      })
   }
 
   const handleDeleteBusiness = id => {
     businessService.deleteOne(id)
-    .then(deletedBusiness => setBusinesses(businesses.filter(business => business._id !== deletedBusiness._id)))
+      .then(deletedBusiness => setBusinesses(businesses.filter(business => business._id !== deletedBusiness._id)))
     navigate('/')
   }
 
   const handleCreateList = (profile, listFormData) => {
     profileService.createList(profile, listFormData)
     profileService.getProfile(profile)
-    .then(updatedProfile => {
-      setProfile(updatedProfile)
-    })
+      .then(updatedProfile => {
+        setProfile(updatedProfile)
+      })
     navigate('/profile')
   }
 
   const handleDeleteList = (profile, listId) => {
     profileService.deleteList(profile, listId)
     profileService.getProfile(profile)
-    .then(updatedProfile => {
-      setProfile(updatedProfile)
-    })
+      .then(updatedProfile => {
+        setProfile(updatedProfile)
+      })
     window.location.reload(false)
   }
 
   const handleAddToList = (profile, list, updatedList) => {
     profileService.addToList(profile, list, updatedList)
     profileService.getProfile(profile)
-    .then(updatedProfile => {
-      setProfile(updatedProfile)
-    })
+      .then(updatedProfile => {
+        setProfile(updatedProfile)
+      })
     window.location.reload(false)
   }
-  
+
   const handleRemoveFromList = (profile, list, business) => {
-    profileService.removeFromList(profile, list, business )
+    profileService.removeFromList(profile, list, business)
     profileService.getProfile(profile)
-    .then(updatedProfile => {
-      setProfile(updatedProfile)
-    })
+      .then(updatedProfile => {
+        setProfile(updatedProfile)
+      })
     window.location.reload(false)
   }
 
@@ -138,54 +136,54 @@ const App = () => {
         />
         <Route
           path="/changePassword"
-          element={user ? <ChangePassword handleSignupOrLogin={handleSignupOrLogin}/> : <Navigate to="/login" />}
+          element={user ? <ChangePassword handleSignupOrLogin={handleSignupOrLogin} /> : <Navigate to="/login" />}
         />
-        
+
         <Route
           path="/create"
-          element={user ? <CreateBusiness  handleAddBusiness={handleAddBusiness}  /> : <Navigate to="/login" />}
-          />
+          element={user ? <CreateBusiness handleAddBusiness={handleAddBusiness} /> : <Navigate to="/login" />}
+        />
 
-          <Route 
+        <Route
           path="/editBusiness"
           element={
-            user ? <EditBusiness  handleEditBusiness={handleEditBusiness}  /> : <Navigate to="/login" />}
-          />
-  
-          <Route
-            path='/'
-            element={
-              user ? <Landing handleDeleteBusiness={handleDeleteBusiness} businesses={businesses} user={user} /> : <Navigate to='/login' />}
-          />
+            user ? <EditBusiness handleEditBusiness={handleEditBusiness} /> : <Navigate to="/login" />}
+        />
 
-          <Route
-            path='/business-details'
-            element={
-              user ? <BusinessDetails  handleAddReview={handleAddReview} handleDeleteReview={handleDeleteReview} handleDeleteBusiness={handleDeleteBusiness} businesses={businesses} user={user} /> : <Navigate to='/login' />} />
+        <Route
+          path='/'
+          element={
+            user ? <Landing handleDeleteBusiness={handleDeleteBusiness} businesses={businesses} user={user} /> : <Navigate to='/login' />}
+        />
 
-          <Route
+        <Route
+          path='/business-details'
+          element={
+            user ? <BusinessDetails handleAddReview={handleAddReview} handleDeleteReview={handleDeleteReview} handleDeleteBusiness={handleDeleteBusiness} businesses={businesses} user={user} /> : <Navigate to='/login' />} />
+
+        <Route
           path='/business-details'
           element={
             user ? <BusinessDetails handleDeleteReview={handleDeleteReview} user={user} /> : <Navigate to='/login' />}
-          />
+        />
 
-          <Route 
-            path='/profile'
-            element={
-              user ? <ProfileDetails handleDeleteList={handleDeleteList} businesses={businesses} user={user} profile={profile} /> : <Navigate to='/login' />}
-          />
+        <Route
+          path='/profile'
+          element={
+            user ? <ProfileDetails handleDeleteList={handleDeleteList} businesses={businesses} user={user} profile={profile} /> : <Navigate to='/login' />}
+        />
 
-          <Route
+        <Route
           path='/addList'
           element={
-            user ? <CreateList businesses={businesses} handleCreateList={handleCreateList}  user={user} /> : <Navigate to='/login' />}
-          />
+            user ? <CreateList businesses={businesses} handleCreateList={handleCreateList} user={user} /> : <Navigate to='/login' />}
+        />
 
-          <Route
+        <Route
           path='/listDetails'
           element={
             user ? <ListDetails handleAddToList={handleAddToList} handleRemoveFromList={handleRemoveFromList} businesses={businesses} user={user} profile={profile} /> : <Navigate to='/login' />}
-          />
+        />
 
       </Routes>
     </>
