a8fb273da647ec1add4a698a16e31f5775a81485
Jeremyhaglerr <jeremyhaglerr@gmail.com>
2022-04-01 11:53:51 -0500

    fix key on landing

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -6,8 +6,8 @@ const Landing = (props) => {
     <div className={styles.container} >
       <h1>All Businesses</h1>
       <div className={styles.bizCard}> {props.businesses?.map (business => ( 
-        <div key={business.id} className={styles.card} >
-          <BusinessCard key={business._id} business={business}
+        <div key={business._id} className={styles.card} >
+          <BusinessCard business={business}
            user={props.user}
            />
         </div>
