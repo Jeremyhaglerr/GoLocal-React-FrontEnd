5aedb246ada2e1e361e00c45e8c6cb083ed19dd2
Jeremyhaglerr <jeremyhaglerr@gmail.com>
2022-04-01 08:20:16 -0500

    fix indent

--- src/components/ListCard/ListCard.jsx
+++ src/components/ListCard/ListCard.jsx
@@ -1,12 +1,12 @@
 import { Link } from 'react-router-dom'
 
-const ListCard = ({list}) => {
-  return ( 
+const ListCard = ({ list }) => {
+  return (
     <>
-      <Link to='/listDetails' key={list.name} state={{list}} >{list.name}</Link> 
+      <Link to='/listDetails' key={list.name} state={{ list }} >{list.name}</Link>
       <br />
     </>
   );
 }
- 
+
 export default ListCard;
